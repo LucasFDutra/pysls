4a351068ed66856a88687fc26512b21946c6ab39
LucasFDutra <lucasfelipedutra@gmail.com>
2020-10-11 09:08:39 -0300

    python_version por variável

--- pysls/tests/create_layer_test.py
+++ pysls/tests/create_layer_test.py
@@ -4,12 +4,14 @@ from pysls.utils.rmdir import rmdir
 import os
 import time
 
+python_version = '3.8'
+
 def test_create_layer_no_req():
     lambda_name = 'lambda_test_layer_no_req'
     layer_test_name = 'numpy_no_req'
-    clf.create_lambda(lambda_name, '3.8')
+    clf.create_lambda(lambda_name, python_version)
 
-    cl.create_layer(layer_test_name, '3.8')
+    cl.create_layer(layer_test_name, python_version)
     files_list = os.listdir('.')
 
     rmdir(os.path.join('.', lambda_name))
@@ -20,12 +22,12 @@ def test_create_layer_req():
     lambda_name = 'lambda_test_layer_req'
     layer_test_name = 'numpy_req'
 
-    clf.create_lambda(lambda_name, '3.8')
+    clf.create_lambda(lambda_name, python_version)
 
     with open(os.path.join('.', 'requirements.txt'), 'w') as requirements:
         requirements.write('numpy')
 
-    cl.create_layer(layer_test_name, '3.8')
+    cl.create_layer(layer_test_name, python_version)
     files_list = os.listdir('.')
     os.remove(os.path.join('.', layer_test_name+'.zip'))
     os.remove(os.path.join('.', 'requirements.txt'))
