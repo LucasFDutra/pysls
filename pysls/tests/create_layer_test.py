ba19c8f64eb8f05ea53dea2ce70d5e54a32e7767
LucasFDutra <lucasfelipedutra@gmail.com>
2020-10-11 10:28:48 -0300

    refactoring testes

--- pysls/tests/create_layer_test.py
+++ pysls/tests/create_layer_test.py
@@ -1,5 +1,5 @@
-import pysls.src.create_lambda as clf
-import pysls.src.create_layer as cl
+from pysls.src.create_lambda import create_lambda
+from pysls.src.create_layer import create_layer
 from pysls.utils.rmdir import rmdir
 import os
 import time
@@ -9,9 +9,9 @@ python_version = '3.8'
 def test_create_layer_no_req():
     lambda_name = 'lambda_test_layer_no_req'
     layer_test_name = 'numpy_no_req'
-    clf.create_lambda(lambda_name, python_version)
+    create_lambda(lambda_name, python_version)
 
-    cl.create_layer(layer_test_name, python_version)
+    create_layer(layer_test_name, python_version)
     files_list = os.listdir('.')
 
     rmdir(os.path.join('.', lambda_name))
@@ -22,12 +22,12 @@ def test_create_layer_req():
     lambda_name = 'lambda_test_layer_req'
     layer_test_name = 'numpy_req'
 
-    clf.create_lambda(lambda_name, python_version)
+    create_lambda(lambda_name, python_version)
 
     with open(os.path.join('.', 'requirements.txt'), 'w') as requirements:
         requirements.write('numpy')
 
-    cl.create_layer(layer_test_name, python_version)
+    create_layer(layer_test_name, python_version)
     files_list = os.listdir('.')
     os.remove(os.path.join('.', layer_test_name+'.zip'))
     os.remove(os.path.join('.', 'requirements.txt'))
