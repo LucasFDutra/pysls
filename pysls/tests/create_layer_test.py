7aa1c8812e1b57b6535c50875625c3a4e7e3ca4c
LucasFDutra <lucasfelipedutra@gmail.com>
2020-10-11 09:07:20 -0300

    refatorando os testes

--- pysls/tests/create_layer_test.py
+++ pysls/tests/create_layer_test.py
@@ -4,17 +4,24 @@ from pysls.utils.rmdir import rmdir
 import os
 import time
 
-lambda_name = 'lambda_test_layer'
-layer_test_name = 'numpy'
-
-clf.create_lambda(lambda_name, '3.8')
-
 def test_create_layer_no_req():
+    lambda_name = 'lambda_test_layer_no_req'
+    layer_test_name = 'numpy_no_req'
+    clf.create_lambda(lambda_name, '3.8')
+
     cl.create_layer(layer_test_name, '3.8')
     files_list = os.listdir('.')
+
+    rmdir(os.path.join('.', lambda_name))
+    
     assert(layer_test_name+'.zip' not in files_list)
 
 def test_create_layer_req():
+    lambda_name = 'lambda_test_layer_req'
+    layer_test_name = 'numpy_req'
+
+    clf.create_lambda(lambda_name, '3.8')
+
     with open(os.path.join('.', 'requirements.txt'), 'w') as requirements:
         requirements.write('numpy')
 
