75642ea4f53bad041d2948aac9d1043dd35f156d
LucasFDutra <lucasfelipedutra@gmail.com>
2020-10-11 20:38:04 -0300

    retirando um print

--- pysls/utils/copy_folder.py
+++ pysls/utils/copy_folder.py
@@ -1,6 +1,5 @@
 import os, shutil
 def copy_folder(src, dst, symlinks=False, ignore=None):
-    print('aqui')
     for item in os.listdir(src):
         s = os.path.join(src, item)
         d = os.path.join(dst, item)
